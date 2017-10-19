e86daab848d065cbedec6b8624e8e00c0d28e14f
Julius Huelsmann <huelsmann@campus.tu-berlin.de>
2017-10-19 07:39:28 +0200

    fixed install vim config. has to use zshell

--- scripts/installvim.sh
+++ scripts/installvim.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 sudo pacman -S mercurial
 cd /mnt/data/repos
 hg clone ssh://hg@bitbucket.org/vim-mirror/vim
@@ -5,22 +7,31 @@ hg clone ssh://hg@bitbucket.org/vim-mirror/vim
 cd vim
 sudo make uninstall
 
-cd src
-make clean
-./configure --with-features=huge \
- 	    - clipboard \
- 	    -clipboard \
-            --enable-multibyte \
-            --enable-rubyinterp=yes \
-            --enable-pythoninterp=yes \
-            --with-python-config-dir=/usr/lib/python2.7/config \
-            --enable-python3interp=yes \
-            --with-python3-config-dir=/usr/lib/python3.5/config \
-            --enable-perlinterp=yes \
-            --enable-luainterp=yes \
-            --enable-gui=gtk2 \
-            --enable-cscope
+
 sudo make uninstall
+make clean
+
+make distclean
+./configure \
+  --with-features=huge \
+  --enable-multibyte \
+  --enable-rubyinterp=yes \
+  --enable-fontset \
+  --enable-largefile \
+  --enable-pythoninterp=dynamic \
+  --with-python-config-dir=/usr/lib/python2.7/config \
+  --enable-python3interp=yes \
+  --enable-python3interp
+  --with-python3-config-dir=/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu \
+  --enable-perlinterp=yes \
+  --enable-luainterp=yes \
+  --enable-gui=gtk2 \
+  --enable-cscope \
+  --with-compiledby="me" \
+  -clipboard \
+  --enable-fail-if-missing
+  #- clipboard 
+
 make -j2 
 sudo make install
 
